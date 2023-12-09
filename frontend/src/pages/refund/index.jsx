5f084a4e6f5420e565de1ba1faf7dcfada2e8ec8
yash-saaspayapp <78974180+yash-deore@users.noreply.github.com>
2023-12-10 01:39:25 +0530

    Added refund form

--- frontend/src/pages/refund/index.jsx
+++ frontend/src/pages/refund/index.jsx
@@ -1,7 +1,27 @@
+import { Box, Button, NumberInput, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import React from "react";
 
 const Refund = () => {
-  return <div>Refund</div>;
+  const form = useForm({
+    initialValues: {},
+  });
+
+  return (
+    <div>
+      <Box maw={340} mx="auto">
+        <form onSubmit={form.onSubmit(console.log)}>
+          <NumberInput label="Event Id" />
+          <NumberInput label="Batch Id" />
+          <NumberInput label="Count" />
+
+          <Button type="submit" mt="sm">
+            Refund
+          </Button>
+        </form>
+      </Box>
+    </div>
+  );
 };
 
 export default Refund;
